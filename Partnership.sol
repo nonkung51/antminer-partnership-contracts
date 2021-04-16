99aa87632995e9cc88c55ba3ef886d4d8f5f1bdd
Nonthakon Jitchiranant <non_thakon@hotmail.com>
2021-04-16 09:59:13 +0700

    Update Partnership.sol

--- Partnership.sol
+++ Partnership.sol
@@ -4,10 +4,19 @@ pragma solidity ^0.8.3;
 
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 
+/**
+ * Some of the code copied from openzeppelin's PaymentSplitter
+ * modify to using ERC20 Token as share and using ERC20 other than ether
+ * check the openzeppelin's contract here: https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/finance/PaymentSplitter.sol
+**/
+
 contract Partnership {
     
     IERC20 shareToken;
     IERC20 btcbToken;
+    
+    uint256 private _totalReleased;
+    mapping(address => uint256) private _released;
 
     constructor(address _shareToken, address _btcbToken) {
         shareToken = IERC20(_shareToken);
@@ -16,16 +25,16 @@ contract Partnership {
     
     function withdraw() public {
         require(shareToken.balanceOf(msg.sender) > 0, "You have no share in this pool!");
-        btcbToken.transfer(msg.sender, _getWithdrawable(msg.sender));
+        uint256 totalReceived = btcbToken.balanceOf(address(this)) + _totalReleased;
+        uint256 payment = totalReceived * shareToken.balanceOf(msg.sender) / shareToken.totalSupply() - _released[msg.sender];
+
+        require(payment != 0, "You already claimed!");
+        _released[msg.sender] = _released[msg.sender] + payment;
+        _totalReleased = _totalReleased + payment;
+        
+        btcbToken.transfer(msg.sender, payment);
     }
-    
-    function _getWithdrawable(address _address) private view returns (uint256) {
-        return getPoolSupply() * shareToken.balanceOf(_address) / shareToken.totalSupply();
-    }
-    
-    function getWithdrawable() public view returns (uint256) {
-        return _getWithdrawable(msg.sender);
-    }
+
     
     function getPoolSupply() public view returns (uint256) {
         return btcbToken.balanceOf(address(this));
